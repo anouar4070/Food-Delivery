fd60d6aaa353615ac2d614576a701578f9d8282f
anouar garbaa <g.anouar@yahoo.com>
2025-09-23 23:07:28 +0200

    complete User Resolver & User Service

---
+++ servers/apps/users/src/user.resolver.ts
@@ -0,0 +1,31 @@
+import { BadRequestException, Query } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UsersService } from './users.service';
+import { RegisterResponse } from './types/user.types';
+import { RegisterDto } from './dto/user.dto';
+import { User } from './entities/user.entity';
+//import { Response } from 'express';
+
+@Resolver('User')
+// @UseFilters
+export class UsersResolver {
+  constructor(private readonly userService: UsersService) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+  ): Promise<RegisterResponse> {
+    if (!registerDto.name || !registerDto.email || !registerDto.password) {
+      throw new BadRequestException('Please fill the all fields');
+    }
+
+    const user = await this.userService.register(registerDto);
+
+    return { user };
+  }
+
+  @Query(() => [User])
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+}
--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
+//import { Response } from 'express';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // private readonly prisma:
+    // private readonly usersRepository: UsersRepository
+    private readonly configService: ConfigService,
+  ) {}
+
+  // register user service
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name,
+      email,
+      password,
+    };
+    return user;
+  }
+
+  // Login service
+  async Login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = { email, password };
+    return user;
+  }
+
+  // get all users service
+  async getUsers() {
+    const users = [
+      {
+        id: '1234',
+        name: 'test',
+        email: 'abc@gmail.com',
+        password: '12345667f',
+      },
+    ];
+    return users;
   }
 }
