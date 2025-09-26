985992cb86f237848c8cdeeb3e64e4889328a201
anouar garbaa <g.anouar@yahoo.com>
2025-09-26 12:25:19 +0200

    Fix Prisma EPERM error on Windows by cleaning and regenerating client

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto, RegisterDto } from './dto/user.dto';
@@ -18,6 +18,14 @@ export class UsersService {
   // register user service
   async register(registerDto: RegisterDto, response: Response) {
     const { name, email, password } = registerDto;
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (isEmailExist) {
+      throw new BadRequestException("User already exist with this email!")
+    }
     const user = await this.prisma.user.create({
       data: {
         name,
