25685966653d9c6aa65e6f529576c2db6d1d74be
anouar garbaa <g.anouar@yahoo.com>
2025-09-27 16:09:33 +0200

    add sendToken utility

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -134,9 +134,35 @@ export class UsersService {
   // Login service
   async Login(loginDto: LoginDto) {
     const { email, password } = loginDto;
-    const user = { email, password };
-    return user;
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user && (await this.comparePassword(password, user.password))) {
+      const tokenSender = new TokenSender(this.configService, this.jwtService);
+      return tokenSender.sendToken(user);
+    } else {
+      return {
+        user: null,
+        accessToken: null,
+        refreshToken: null,
+        error: {
+          message: 'Invalid email or password',
+        },
+      };
+    }
   }
+
+  // compare with hashed password
+  async comparePassword(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+
 
   // get all users service
   async getUsers() {
