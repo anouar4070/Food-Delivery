0832bf73b0d325a587214663f0b1123e5957106a
anouar garbaa <g.anouar@yahoo.com>
2025-09-28 02:06:39 +0200

    Tailwind + HeroUI  Stup

--- clients/user-ui/src/components/Layout/Header.jsx
+++ clients/user-ui/src/components/Layout/Header.jsx
@@ -1,7 +1,12 @@
+"use client";
+
 import React from "react";
+import { Button } from "@heroui/react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return  <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Button color="primary">Hello HeroUI from Header</Button>
+    </main>;
 };
 
 export default Header;
