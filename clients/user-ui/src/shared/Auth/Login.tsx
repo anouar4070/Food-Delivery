40d482a761f412e36f6378d2fe931287ddb904c9
anouar garbaa <g.anouar@yahoo.com>
2025-10-03 15:15:23 +0200

    signup model

--- clients/user-ui/src/shared/Auth/Login.tsx
+++ clients/user-ui/src/shared/Auth/Login.tsx
@@ -1,6 +1,8 @@
 import styles from "@/src/utils/style";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { AiFillGithub, AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 import { z } from "zod";
 
 const formSchema = z.object({
@@ -10,7 +12,7 @@ const formSchema = z.object({
 
 type LoginSchema = z.infer<typeof formSchema>;
 
-const Login = () => {
+const Login = ({setActiveState}: {setActiveState: (e: string) => void}) => {
   const {
     register,
     handleSubmit,
@@ -74,7 +76,7 @@ const Login = () => {
           {errors.password && (
             <span className="text-red-500">{`${errors.password.message}`}</span>
           )}
-          {/* {!show ? (
+          {!show ? (
             <AiOutlineEyeInvisible
               className="absolute bottom-3 right-2 z-1 cursor-pointer"
               size={20}
@@ -86,17 +88,40 @@ const Login = () => {
               size={20}
               onClick={() => setShow(false)}
             />
-          )} */}
+          )}
         </div>
 
         <div className="w-full mt-5">
-         <input
-         type="submit"
-         value="login"
-         disabled={isSubmitting}
-         className={`${styles.button}`}
-         />
+          <span
+            className={`${styles.label} text-[#2190ff] block text-right cursor-pointer`}
+            // onClick={() => setActiveState("Forgot-Password")}
+          >
+            Forgot your password?
+          </span>
+          <input
+            type="submit"
+            value="login"
+            disabled={isSubmitting}
+            className={`${styles.button} mt-3`}
+          />
         </div>
+        <br />
+        <h5 className="text-center pt-4 font-Poppins text-[16px] text-white">
+          Or join with
+        </h5>
+        <div className="flex items-center justify-center my-3">
+          <FcGoogle size={30} className="cursor-pointer mr-2" />
+          <AiFillGithub size={30} className="cursor-pointer ml-2" />
+        </div>
+        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+          Not have any account?
+          <span
+            className="text-[#2190ff] pl-1 cursor-pointer"
+            onClick={() => setActiveState("Signup")}
+          >
+            Sign up
+          </span>
+        </h5>
         <br />
       </form>
     </div>
