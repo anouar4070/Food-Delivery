3a2104070a3e1e622bb165714f18239a36a764e3
anouar garbaa <g.anouar@yahoo.com>
2025-10-05 19:01:35 +0200

    UI: closing the Module

--- clients/user-ui/src/screens/AuthScreen.tsx
+++ clients/user-ui/src/screens/AuthScreen.tsx
@@ -8,17 +8,17 @@ import { useState } from "react";
 const AuthScreen = ({ setOpen }: { setOpen: (e: boolean) => void }) => {
   const [activeState, setActiveState] = useState("Login");
 
-  // const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
-  //   if (e.target instanceof HTMLDivElement && e.target.id === "screen") {
-  //     setOpen(false);
-  //   }
-  // };
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (e.target instanceof HTMLDivElement && e.target.id === "screen") {
+      setOpen(false);
+    }
+  };
 
   return (
     <div
       className="w-full fixed top-0 left-0 h-screen z-50 flex items-center justify-center bg-[#00000027]"
-      // id="screen"
-      // onClick={handleClose}
+     id="screen"
+     onClick={handleClose}
     >
       <div className="w-[500px] bg-slate-900 rounded shadow-sm p-3">
         {activeState === "Login" && (
