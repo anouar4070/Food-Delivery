0f4f215cd35a7f1b82c40a31d53e7e952e20fafe
anouar garbaa <g.anouar@yahoo.com>
2025-10-06 14:57:12 +0200

    Get Logged in user & logout Part 1

--- clients/user-ui/src/graphql/gql.setup.ts
+++ clients/user-ui/src/graphql/gql.setup.ts
@@ -1,4 +1,15 @@
-import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
+import Cookies from "js-cookie";
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      accesstoken: Cookies.get("access_token"),
+      refreshtoken: Cookies.get("refresh_token"),
+    },
+  });
+  return forward(operation);
+});
 
 console.log(process.env.NEXT_PUBLIC_SERVER_URI);
 
